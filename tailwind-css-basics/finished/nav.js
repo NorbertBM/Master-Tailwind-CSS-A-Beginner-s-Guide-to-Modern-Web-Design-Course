c4d9256056b9a49d06c64c5fd7e59dcb3723a71e
NorbertBM <menyhart.b.norbert@gmail.com>
2025-03-20 17:52:50 +0100

    basics

--- tailwind-css-basics/finished/nav.js
+++ tailwind-css-basics/finished/nav.js
@@ -67,7 +67,7 @@ function createNav() {
   const navLinks = document.createElement("div");
   navLinks.classList.add(
     "hidden",
-    "md:flex",
+    "xl:flex",
     "flex-col",
     "h-full",
     "border",
@@ -124,7 +124,7 @@ function createNav() {
   });
 
   const hamburger = document.createElement("button");
-  hamburger.classList.add("md:hidden");
+  hamburger.classList.add("xl:hidden");
   hamburger.innerHTML = `
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
